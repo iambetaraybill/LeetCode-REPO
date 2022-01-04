5cfbebad3f81e5b040728e92cb4eb997036108a9
Arpan Sarkar <45901177+iambetaraybill@users.noreply.github.com>
2022-01-04 18:05:41 +0000

    Added optimal js solution

--- Longest Substring Without Repeating Characters/longest-substring-without-repeating-characters.cs
+++ Longest Substring Without Repeating Characters/longest-substring-without-repeating-characters.cs
@@ -21,3 +21,26 @@ public class Solution {
         return longest;
     }
 }
+
+//Optimal js solution
+// var lengthOfLongestSubstring = function(s) {
+//      if(s.length <= 1) return s.length;
+    
+//     const seen = {};
+//     let left = 0, longest = 0;
+    
+//     for(let right = 0; right < s.length; right++) {
+//         const currentChar = s[right];
+//         const previouslySeenChar = seen[currentChar];
+        
+//         if(previouslySeenChar >= left) {
+//           left = previouslySeenChar + 1;
+//         }
+        
+//         seen[currentChar] = right;
+        
+//         longest = Math.max(longest, right - left + 1);
+//     }
+    
+//     return longest;
+// };
