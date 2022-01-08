46cfc5e2478b03ce28f6ba74d169e5a97489fabc
Arpan Sarkar <45901177+iambetaraybill@users.noreply.github.com>
2022-01-08 16:11:14 +0000

    Reverse linked list

---
+++ Reverse Linked List/README.md
@@ -0,0 +1,17 @@
+Given the head of a singly linked list, reverse the list, and return the reversed list.</br>
+![alt text](https://assets.leetcode.com/uploads/2021/02/19/rev1ex1.jpg)</br>
+Input: head = [1,2,3,4,5]</br>
+Output: [5,4,3,2,1]</br>
+![alt test ](https://assets.leetcode.com/uploads/2021/02/19/rev1ex2.jpg)
+Input: head = [1,2]</br>
+Output: [2,1]</br>
+Example 3:</br>
+
+Input: head = []</br>
+Output: []</br>
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
---
+++ Reverse Linked List/ReverseLinkedList.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        
+        while(current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
